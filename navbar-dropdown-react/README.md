8f1b1345fe2150efab1850002a9d779fefa2dbe7
MateusOli21 <mateus.oli.abreu@hotmail.com>
2020-11-25 19:03:53 -0300

    Initial commit.

---
+++ navbar-dropdown-react/README.md
@@ -0,0 +1,9 @@
+# Navbar Dropdown React
+
+Um projeto simples para criar um website com uma barra de navegação com submenus suspensos.
+
+### Tecnologias
+
+- React JS
+- Styled Components
+- React Router Dom
---
+++ navbar-dropdown-react/src/styles/GlobalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+*, input, button{
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+  font-family: -apple-system, 'Roboto', Ubuntu, 'Open Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+
+:root{
+    --white-color: #fff;
+    --black-color: #131313;
+    --orange-color: #ff7062;
+}
+
+`;
